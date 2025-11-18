7d450948591132547dc010771628e2ed7209bb90
MRRazvan94 <mateiu.raul.razvan@gmail.com>
2025-11-18 21:20:08 +0200

    Create git.py
    First commit: added git.py

---
+++ git.py
@@ -0,0 +1,8 @@
+class plm:
+    def __init__(self):
+        x=int(input("Enter a number: "))
+        if x<10:
+            print("The number is less than 10.")
+        else:
+            print("The number is 10 or greater.")
+y=plm()
