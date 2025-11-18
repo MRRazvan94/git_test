30c4e2b55999a8d53e6bcc51c53b3516c899ee9a
MRRazvan94 <mateiu.raul.razvan@gmail.com>
2025-11-18 21:25:19 +0200

    Update git.py
    Update git.py message to version 2

--- git.py
+++ git.py
@@ -2,7 +2,7 @@ class plm:
     def __init__(self):
         x=int(input("Enter a number: "))
         if x<10:
-            print("The number is less than 10.")
+            print("v.2.0 The number is less than 10.")
         else:
-            print("The number is 10 or greater.")
+            print("v.2.0 The number is 10 or greater.")
 y=plm()
